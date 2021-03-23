acbb0bf31f7e2d1581e2cd39533cc0f3f1f49381
Laura Figueroa <lfigu042@fiu.edu>
2021-03-22 21:17:58 -0400

    Creates two nodes with a GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -67,10 +68,48 @@ public class Main {
      * Implements a Graph. Uses an adjacency matrix to represent the graph. *
      * * @author Prof. Antonio Hernandez
      */
-//    public class Graph implements GraphInterface {
-//        private int verticesNumber;
-//        private int[][] matrix; //adjacency matrix    public Graph()    {        verticesNumber = 5;        matrix = new int[verticesNumber][verticesNumber];    }    public Graph(int n)    {        verticesNumber = n;        matrix = new int[verticesNumber][verticesNumber];    }    public void addEdge(int v, int w)    {        matrix[v][w] = 1;        matrix[w][v] = 1;    }    public void removeEdge(int v, int w)    {        matrix[v][w] = 0;        matrix[w][v] = 0;            }    /**     * Finds vertices adjacent to a given vertex.     *      * @param v given vertex     * @return list of vertices adjacent to v stored in an array;     *          size of array = number of adjacent vertices     */    public int[] findAdjacencyVertices(int v)    {        int[] vert = new int[verticesNumber];        int total = 0;        for (int i=0; i<verticesNumber; i++)        {            if (matrix[v][i] != 0)            {                vert[total] = i;                total++;            }        }        return Arrays.copyOf(vert, total);
-//    }
+    public class Graph implements GraphInterface {
+        private int verticesNumber;
+        private int[][] matrix; //adjacency matrix
+
+        public Graph() {
+            verticesNumber = 5;
+            matrix = new int[verticesNumber][verticesNumber];
+        }
+
+        public Graph(int n) {
+            verticesNumber = n;
+            matrix = new int[verticesNumber][verticesNumber];
+        }
+
+        public void addEdge(int v, int w) {
+            matrix[v][w] = 1;
+            matrix[w][v] = 1;
+        }
+
+        public void removeEdge(int v, int w) {
+            matrix[v][w] = 0;
+            matrix[w][v] = 0;
+        }
+
+        /**
+         * Finds vertices adjacent to a given vertex.
+         * * @param v given vertex
+         * * @return list of vertices adjacent to v stored in an array;
+         * * size of array = number of adjacent vertices
+         */
+        public int[] findAdjacencyVertices(int v) {
+            int[] vert = new int[verticesNumber];
+            int total = 0;
+            for (int i = 0; i < verticesNumber; i++) {
+                if (matrix[v][i] != 0) {
+                    vert[total] = i;
+                    total++;
+                }
+            }
+            return Arrays.copyOf(vert, total);
+        }
+    }
 
 //    public String toString() {
 //        String s = "";
@@ -103,7 +142,23 @@ public class Main {
             int height = 50;
             int labelX = 17;
             int labelY = 31;
-            int gridWidth = 150;        //draw vertex 1        g.setColor(Color.ORANGE);        g.fillOval(leftX, topY, width, height);        g.setColor(Color.BLACK);        g.drawOval(leftX, topY, width, height);        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));        g.drawString("1", leftX+labelX, topY+labelY);        //draw vertex 2        g.setColor(Color.ORANGE);        g.fillOval(leftX+gridWidth, topY, width, height);        g.setColor(Color.BLACK);        g.drawOval(leftX+gridWidth, topY, width, height);        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));        g.drawString("2", leftX+gridWidth+labelX, topY+labelY);        //draw edge (1, 2)        g.setColor(Color.BLACK);        g.drawLine(leftX+width, topY+height/2, leftX+gridWidth, topY+height/2);    }}
+            int gridWidth = 150;        //draw vertex 1
+            g.setColor(Color.ORANGE);
+            g.fillOval(leftX, topY, width, height);
+            g.setColor(Color.BLACK);
+            g.drawOval(leftX, topY, width, height);
+            g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+            g.drawString("1", leftX + labelX, topY + labelY);        //draw vertex 2
+            g.setColor(Color.ORANGE);
+            g.fillOval(leftX + gridWidth, topY, width, height);
+            g.setColor(Color.BLACK);
+            g.drawOval(leftX + gridWidth, topY, width, height);
+            g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+            g.drawString("2", leftX + gridWidth + labelX, topY + labelY);        //draw edge (1, 2)
+            g.setColor(Color.BLACK);
+            g.drawLine(leftX + width, topY + height / 2, leftX + gridWidth, topY + height / 2);
         }
     }
 }
+
+
