f97d79c58afa0bf4ed4d5a5139b88b098a28238d
Laura Figueroa <lfigu042@fiu.edu>
2021-03-25 16:42:19 -0400

    created matrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -96,32 +96,42 @@ public class Main {
         int mat_i_j = 0;
         int[][] graph_matrix;
         int s,t; //start and target
-        Graph my_graph; //create graph with parameters from file
+
         while (in.hasNextLine()) {
             numberOfVertices = in.nextInt();
             System.out.println("Number of vertices: " + numberOfVertices);
             graph_matrix = new int[numberOfVertices][numberOfVertices]; //creating 2d array for file matrix
 
-            System.out.println("Matrix: ");
+//          //populating matrix
             for (int i = 0; i < numberOfVertices; i++) {
                 for (int j = 0; j < numberOfVertices; j++) {
                     mat_i_j = in.nextInt();
-                    System.out.print(mat_i_j + " ");
                     graph_matrix[i][j] = mat_i_j;
                 }
-                System.out.println();
             }
+            //print matrix:
+            System.out.println("Matrix:  ");
+            for(int[] row : graph_matrix)
+                printRow(row);
+
+//          get 's' and 't'
             s = in.nextInt();
             t = in.nextInt();
             System.out.print("S and T are:  ");
             System.out.println(s + " " + t);
-            my_graph = new Graph(numberOfVertices, graph_matrix); //how to send this off?
-            algo_dijkstra(graph_matrix, s, t);
+
         }
 
         in.close();
+//        return graph_matrix;
     }
-
+    public static void printRow(int[] row) {
+        for (int i : row) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.println();
+    }
     public interface GraphInterface {
         public void addEdge(int v, int w);
 
@@ -173,15 +183,7 @@ public class Main {
             matrix[i][j] = 0;
             matrix[j][i] = 0;
         }
-        public void printMatrix(){
-            System.out.println("Printing matrix...");
-            for (int i = 0; i < verticesNumber; i++) {
-                for (int j = 0; j < verticesNumber; j++) {
-                    System.out.print(matrix[i][j]+ " ");
-                }
-                System.out.println();
-            }
-        }
+
         /**
          * Finds vertices adjacent to a given vertex.
          * * @param v given vertex
@@ -199,6 +201,9 @@ public class Main {
             }
             return Arrays.copyOf(vert, total);
         }
+        public void printMatrix(){
+            System.out.println("Printing matrix maybe");
+        }
     }
 
     public String toString(int[][] matrix, int verticesNumber) {
@@ -213,8 +218,8 @@ public class Main {
     }
 
     public static class FrameDisplay extends JFrame {
-        int WIDTH = 450;
-        int HEIGHT = 450;
+        int WIDTH = 600;
+        int HEIGHT = 600;
 
         public FrameDisplay() {
             setTitle("Graph Display");
@@ -225,6 +230,7 @@ public class Main {
     }
 
     public static class GraphDisplay extends JPanel {
+//        matrix =  readGraph();
         public void paint(Graphics g) {
             int leftX = 100;
             int topY = 100;
@@ -248,6 +254,7 @@ public class Main {
             g.setColor(Color.BLACK);
             g.drawLine(leftX + width, topY + height / 2, leftX + gridWidth, topY + height / 2);
         }
+
     }
 }
 
